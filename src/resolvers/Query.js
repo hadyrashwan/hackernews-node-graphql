b36471090f3995e24bde826fc84ef55401f7040c
Hady Rashwan <h2rashwan@gmail.com>
2020-10-09 21:43:33 +0200

    Adding sort support

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,21 +1,22 @@
 async function feed(parent, args, context, info) {
-    const where = args.filter
-      ? {
+  const where = args.filter
+    ? {
         OR: [
           { description: { contains: args.filter } },
           { url: { contains: args.filter } },
         ],
       }
-      : {}
-  
-    const links = await context.prisma.link.findMany({
-      where,
-      skip: args.skip,
-      take: args.take,
-    })
-  
-    return links
-  }
+    : {};
+
+  const links = await context.prisma.link.findMany({
+    where,
+    skip: args.skip,
+    take: args.take,
+    orderBy: args.orderBy,
+  });
+
+  return links;
+}
 
 module.exports = {
   feed,
